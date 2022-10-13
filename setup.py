0210d7526703597a08cbb6f51ef44a4b1c50c9e2
lacava <williamlacava@gmail.com>
2022-10-13 10:30:13 -0400

    adds setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='fomo',
+      version='0.0',
+      description='Fairness oriented Multi-objective Optimization',
+      author='William La Cava',
+      author_email='williamlacava@gmail.com',
+      url='https://github.com/cavalab/fomo',
+      packages=['fomo']
+     )
