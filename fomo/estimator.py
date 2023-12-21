06ca9cf85b4dc99d6d1474ea0a74f8f205f5811f
lacava <williamlacava@gmail.com>
2023-12-21 12:07:21 -0500

    get pareto points function

--- fomo/estimator.py
+++ fomo/estimator.py
@@ -34,6 +34,7 @@ import copy
 import math
 import uuid 
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
@@ -339,11 +340,7 @@ class FomoEstimator(BaseEstimator):
         check_is_fitted(self, 'is_fitted_')
         I = self.I_
         F = self._get_signed_F()
-        axis_labels = (
-            [ am._score_func.__name__ for am in self.accuracy_metrics_ ] 
-            + [ fn.__name__ for fn in self.fairness_metrics_ ]
-        )
-        axis_labels = [al.replace('_',' ') for al in axis_labels]
+        axis_labels = self._get_objective_names()
         plot = (
             Scatter()
             .add(F, alpha=0.2, label='Candidate models')
@@ -367,12 +364,21 @@ class FomoEstimator(BaseEstimator):
                 F[:,i] = F[:,i]*m._sign
         return F
 
+    def _get_objective_names(self):
+        """Returns names of functions defining the objectives"""
+        labels = (
+            [ m._score_func.__name__ for m in self.accuracy_metrics_ ] 
+            + [ fn.__name__ for fn in self.fairness_metrics_ ]
+        )
+        labels = [l.replace('_',' ') for l in labels]
+        return labels
+
     def get_pareto_points(self):
         """Return a Pandas dataframe of the Pareto archive points"""
-        archive = pd.DataFrame(
-            self._get_signed_F(), 
-            columns=self.accuracy_metrics_ + self.fairness_metrics_
-        )
+        F = self._get_signed_F() 
+        I = self.I_
+        archive = pd.DataFrame(F, columns=self._get_objective_names())
+        archive['chosen'] = [all(f==F[I]) for f in F]
         return archive
 
 class FomoClassifier(FomoEstimator, ClassifierMixin, BaseEstimator):
